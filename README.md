ad97df37921620ea7fe615384a0bf681a3721e95
dongdong.wu <withwudongdong@gmail.com>
2022-03-04 14:55:46 +0800

    2中方案提交

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # UserIsUseApp
 判断用户使用操作App 监听方案 讨论
+
+# 第一种方案
+
+可以直接监听`Activity`的`dispatchTouchEvent`方法。因为事件开始，是从`Activity`的`dispatchTouchEvent`方法开始分发的。
+
+# 第二种方案
+
+给`Looper`中设置`Logging`
--- app/src/main/java/com/dongdongwu/userisuseapp/MainActivity.kt
+++ app/src/main/java/com/dongdongwu/userisuseapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.dongdongwu.userisuseapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.util.Printer
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var currentTime = 0L
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (MotionEvent.ACTION_DOWN == ev?.action) {
+            Log.e("123===", "dispatchTouchEvent")
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mLogging = Printer {
+            val nowTime = System.currentTimeMillis()
+            if (it.startsWith(">>>>> Dispatching to ") && (nowTime - currentTime > 1000)) {
+                currentTime = nowTime
+                Log.e("123===", "-----$it")
+            }
+        }
+        Looper.getMainLooper().setMessageLogging(mLogging)
     }
 }
